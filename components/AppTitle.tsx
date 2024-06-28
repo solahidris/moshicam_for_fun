0392f7cbeb703264575fc0eeeca25e67b8be0c12
solahidris <emailsolah@gmail.com>
2024-06-28 23:14:06 +0800

    update

--- components/AppTitle.tsx
+++ components/AppTitle.tsx
@@ -6,7 +6,9 @@ const AppTitle = () => {
         <main className="flex items-center justify-center p-6 border-b shadow-md">
             <div className="flex flex-col">
                 <p className="font-semibold text-5xl lg:text-7xl">moshicam</p>
-                <p className="font-semibold text-sm lg:text-base">unofficial fan build tool</p>
+                <div>
+                    <span className="font-semibold text-sm lg:text-base">unofficial fan build tool</span>
+                </div>
             </div>
             <Image src="/moshi_logo.svg" alt="Moshi Logo" width={100} height={100} className="w-16 lg:w-20 lg:w-20 ml-2"/>
         </main>
